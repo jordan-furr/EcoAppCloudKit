0c0fcfbc08a68ae951bfeee12fb8db7a85eaf323
Jordan Furr <you@example.com>
2020-04-19 23:22:34 -0400

    add plus button working and basic taskviewcontroller working

---
+++ EcoAppCloudKit/Controllers/View Controller/HabitTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  HabitTableViewCell.swift
+//  EcoAppCloudKit
+//
+//  Created by Jordan Furr on 4/19/20.
+//  Copyright © 2020 Jordan Furr. All rights reserved.
+//
+
+import UIKit
+
+protocol HabitTableViewCellDelegate: class {
+    func tappedButton(for cell: HabitTableViewCell)
+}
+
+class HabitTableViewCell: UITableViewCell {
+    
+    var habit: Habit?
+    weak var delegate: HabitTableViewCellDelegate?
+    
+    //MARK: - IB Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    //MARK: - IB Actions
+    @IBAction func plusButtonTapped(_ sender: Any) {
+        delegate?.tappedButton(for: self)
+    }
+    
+    //MARK: - Helper Funcs
+    func setHabit(habit: Habit){
+        self.habit = habit
+        updateUI()
+    }
+    
+    @objc func updateUI(){
+        guard let habit = habit else {return}
+        titleLabel.text = habit.title
+        counterLabel.text = "\(habit.counter ?? 0)"
+    }
+}
--- EcoAppCloudKit/Controllers/View Controller/HomeTableViewController.swift
+++ EcoAppCloudKit/Controllers/View Controller/HomeTableViewController.swift
@@ -23,57 +23,39 @@ class HomeTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
-        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath) as? HabitTableViewCell else {return UITableViewCell()}
         let habit = HabitController.shared.chosenHabits[indexPath.row]
-        cell.textLabel?.text = habit.title
+        cell.setHabit(habit: habit)
+        cell.delegate = self
         return cell
     }
+     
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "toDetail" {
+            guard let indexPath = tableView.indexPathForSelectedRow,
+            let destinationVC = segue.destination as? TaskViewController else {return}
+            let habit = HabitController.shared.chosenHabits[indexPath.row]
+            destinationVC.habit = habit
+        }
     }
-    */
-
 }
+
+extension HomeTableViewController: HabitTableViewCellDelegate {
+    func tappedButton(for cell: HabitTableViewCell) {
+        guard let habit = cell.habit else {return}
+        habit.counter = habit.counter! + 1
+        cell.updateUI()
+        HabitController.shared.updateCounter(habit: habit) { ( result) in
+            print("counter update saved")
+        }
+    }
+}
+
